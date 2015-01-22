84940b173746c24493e9be2af4e6e37de6934fa9
Jarod Reyes <jarodreyes@gmail.com>
2015-01-21 23:23:56 -0800

    Removed some more scruff.

--- app.rb
+++ app.rb
@@ -19,24 +19,6 @@ before do
   @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 end
 
-def sendMessage(from, to, body, media)
-  if media.nil?
-    message = @client.account.messages.create(
-      :from => from,
-      :to => to,
-      :body => body
-    )
-  else
-    message = @mmsclient.messages.create(
-      :from => from,
-      :to => to,
-      :body => body,
-      :media_url => media,
-    )
-  end
-  puts message.to
-end
-
 get "/" do
   haml :index
 end
@@ -81,7 +63,7 @@ route :get, :post, '/branded-sms' do
 end
 
 route :get, :post, '/points' do
-  puts 'points/'
+
   if params[:phone_number].nil?
     @phone_number = Sanitize.clean(params[:From])
   else
@@ -125,38 +107,4 @@ get '/mms-demo' do
   )
   puts message.to
   halt 200
-end
-
-# Generic webhook to send sms from 'TWILIO'
-get '/sms-hook' do
-  @user = params[:to]
-  if params[:msg].nil?
-    @msg = 'Congrats you have just sent an SMS with just a few lines of code.'
-  else
-    @msg = params[:msg]
-  end
-  message = @mmsclient.messages.create(
-    :from => 'TWILIO',
-    :to => @user,
-    :body => @msg,
-    :media_url => "http://baby-notifier.herokuapp.com/img/sms-pic.png",
-  )
-  puts message.to
-  halt 200
-end
-
-# Receive messages twilio app endpoint - inbound
-route :get, :post, '/christmas' do
-  @xmas_number = ENV['XMAS_NUMBER']
-  @phone_number = Sanitize.clean(params[:From])
-  @body = params[:Body]
-  @message = "Congratulations and Happy Holidays! You are receiving this message because you found the #12HacksOfChristmas easter-egg. For your efforts you will be greatly rewarded this winter. If you'd like to receive your prize, simply respond with your name and mailing address."
-  sendMessage @xmas_number, @phone_number, @message, nil
-end
-
-# Receive messages twilio app endpoint - inbound
-route :get, :post, '/christmas-voice' do
-  Twilio::TwiML::Response.new do |r|
-    r.Say "Congratulations! You are receiving this message because you found the twelve hacks of christmas easter-egg. For your efforts you will be greatly rewarded this winter. If you'd like to receive your prize, please send an SMS to this number with your name and mailing address. Happy Holidays! ", loop: 2, voice: 'alice', language: 'en-GB'
-  end.text
 end
--- environments.rb
+++
@@ -1,7 +0,0 @@
-configure :development do
-  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/dev.db")
-end
-
-configure :production do
-  DataMapper.setup(:default, 'postgres://user:password@hostname/database')
-end
